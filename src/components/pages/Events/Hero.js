98449e93b83e7f4793771714098824174bc2624a
shakil <shakil.dgency@gmail.com>
2025-02-17 18:36:52 +0600

    refactor: image length issue in hero section on event page

--- src/components/pages/Events/Hero.js
+++ src/components/pages/Events/Hero.js
@@ -5,10 +5,10 @@ import BigButton from "@/components/buttons/BigButton";
 
 function Hero() {
 	return (
-		<div className="h-[950px] w-full bg-[url('/pages/Event/eventHero.png')] bg-no-repeat object-cover relative mb-[200px]">
-			{/* <Image src="/pages/event/eventHero.png" height={950} width={1980} alt="" className="h-full w-full object-cover" /> */}
-			<div className=" flex justify-center">
-				<div className="absolute -bottom-[150px]">
+		<div className="h-[950px] w-full bg-[url('/pages/Event/eventHero.png')] bg-no-repeat bg-cover bg-center relative mb-[200px]">
+			{/* <Image src="/pages/Event/eventHero.png" height={950} width={1980} alt="" className="h-full w-full object-cover" /> */}
+			<div className=" flex justify-center ">
+				<div className="absolute z-10 -bottom-[150px]">
 					<SectionStarter
 						title1="Epic Escape Room Birthday Party in Hollywood"
 						description="Celebrate your special day with an action-packed escape room birthday party in Hollywood, LA! Immersive challenges, thrilling fun, and unforgettable memories await at Quest Room"
@@ -19,6 +19,7 @@ function Hero() {
 					</div>
 				</div>
 			</div>
+			<div className="absolute bottom-0 h-[600px] w-full bg-gradient-to-t from-[#000A19] from-5%  to-transparent to-95%  "></div>
 		</div>
 	);
 }
